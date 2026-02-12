1cfbd70e9ef3cc4e043ddab72b1faf5ff4f29560
Robert Hadsell <rhadsell@esri.com>
2026-02-12 17:11:57 -0600

    Remove S3 backend configuration from provider.tf
    Removed backend configuration for S3 state storage.

--- provider.tf
+++ provider.tf
@@ -3,13 +3,7 @@ terraform {
    aws = {
      source  = "hashicorp/aws"
      version = "~> 4.18.0"
-   }
- }
-
- backend "s3" {
-   bucket = "tf-tuts-state"
-   key    = "state"
-   region = "eu-central-1"
- }
+    }
+  }
 }
 
